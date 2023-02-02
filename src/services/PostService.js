6d51a60e5809c49a472a463925d2a24810aa23b4
parkchanbin54 <parks4754@gmail.com>
2023-02-02 09:36:09 +0900

    update

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -7,42 +7,42 @@ const BACKEND_API_BASE_URL="http://localhost:8080";
 
 
 export async function getPosts(){
-    const response=await axios.get(`${BACKEND_API_BASE_URL}/post`)
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/post`)
     return response.data.result;
 }
 
 export async function getMyPost(userId){
-    const response=await axios.get(`${BACKEND_API_BASE_URL}/post/${userId}`)
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/post/${userId}`)
     return response.data.result;
 }
 
 export async function getProject(userId){
-    const response=await axios.get(`${BACKEND_API_BASE_URL}/project/${userId}`)
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/project/${userId}`)
     return response.data.result;
 }
 
 export async function getRequestedPost(userId){
-    const response=await axios.get(`${BACKEND_API_BASE_URL}/requestedPost/${userId}`)
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/requestedPost/${userId}`)
     return response.data.result;
 }
 
 export async function getTemperatures(){
-    const response = await axios.get(`${BACKEND_API_BASE_URL}/temperature`)
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/temperature`)
     return response.data.result;
 }
 
 export async function getApplicant(postId){
-    const response = await axios.get(`${BACKEND_API_BASE_URL}/applicant/${postId}`)
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/applicant/${postId}`)
     return response.data.result;
 }
 
 export async function getCurrentPostCategory(postId){
-    const response = await axios.get(`${BACKEND_API_BASE_URL}/currentParticipant/${postId}`)
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/currentParticipant/${postId}`)
     return response.data.result;
 }
 
 export async function createApplicant(postId,request){
-    const response = await axios.post(`${BACKEND_API_BASE_URL}/registerApplicant`, {
+    const response = await axios.post(`${BACKEND_API_BASE_URL}/api/registerApplicant`, {
         postId: postId,
         userId: localStorage.getItem('memberId'),
         request: request
@@ -51,7 +51,7 @@ export async function createApplicant(postId,request){
 }
 
 export async function createParticipant(postId,name){
-    const response = await axios.post(`${BACKEND_API_BASE_URL}/registerParticipant`, {
+    const response = await axios.post(`${BACKEND_API_BASE_URL}/api/registerParticipant`, {
         postId: postId,
         name: name
     })
@@ -59,33 +59,33 @@ export async function createParticipant(postId,name){
 }
 
 export async function deleteApplicant(postId,name){
-    const response = await axios.delete(`${BACKEND_API_BASE_URL}/applicant/${postId}/${name}`)
+    const response = await axios.delete(`${BACKEND_API_BASE_URL}/api/applicant/${postId}/${name}`)
     return response.data.code;
 }
 
 export async function getParticipant(postId,category){
-    const response = await axios.get(`${BACKEND_API_BASE_URL}/applicant/${postId}/${category}`)
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/applicant/${postId}/${category}`)
     return response.data.result;
 }
 
 export async function deleteMyPost(postId){
     const userId = localStorage.getItem("memberId")
-    const response = await axios.delete(`${BACKEND_API_BASE_URL}/post/${postId}/${userId}`)
+    const response = await axios.delete(`${BACKEND_API_BASE_URL}/api/post/${postId}/${userId}`)
     return response.data.code;
 }
 
 export async function updatePostView(postId){
-    const response = await axios.patch(`${BACKEND_API_BASE_URL}/post/${postId}`)
+    const response = await axios.patch(`${BACKEND_API_BASE_URL}/api/post/${postId}`)
     return response.data.result;
 }
 
 export async function updateCurrentCategory(postId, name){
-    const response = await axios.patch(`${BACKEND_API_BASE_URL}/postCategory/${postId}/${name}`)
+    const response = await axios.patch(`${BACKEND_API_BASE_URL}/api/postCategory/${postId}/${name}`)
     return response.data.code;
 }
 
 export async function updatePostStatus(postId){
-    const response = await axios.patch(`${BACKEND_API_BASE_URL}/postStatus/${postId}`)
+    const response = await axios.patch(`${BACKEND_API_BASE_URL}/api/postStatus/${postId}`)
     return response.data.result;
 }
 
