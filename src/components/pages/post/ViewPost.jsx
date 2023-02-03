fe39654c1fff95d141d55402bb43a5df1cc1334d
hojin19082 <905018@naver.com>
2023-02-03 15:10:17 +0900

    change applicant & participant logic (localhost)

--- src/components/pages/post/ViewPost.jsx
+++ src/components/pages/post/ViewPost.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect} from "react";
 import { useNavigate, useParams, useLocation } from "react-router-dom";
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue} from 'recoil';
 import { getApplicant, deleteMyPost, updatePostView, updatePostStatus, getCurrentPostCategory } from "../../../services/PostService";
 import Modal from "./Modal";
 import { userState } from '../../../atom';
-import axios from 'axios';
-
-
-
-
-
 
 function ViewPost() {
     const navigate = useNavigate();
@@ -21,6 +15,7 @@ function ViewPost() {
     const [applicant, setApplicant] = useState([]);
     const [currentCategory, setCurrentCategory] = useState({});
     const [clickedCategory, setClickedCategory] = useState([]);
+    
     //지금은 각각 모달 state를 만들었지만 전역 모달로 바꿔서 해도 될듯
     const [registerModalOpen, setRegisterModalOpen] = useState(false);
     const [approveModalOpen, setApproveModalOpen] = useState(false);
@@ -88,34 +83,34 @@ function ViewPost() {
 
     }, []);
 
-    function getPortInfo(userId){
+    // function getPortInfo(userId){
     
-        axios.get(`/api/user/info/${userId}`)
-            .then((response) => {
-                console.log('getgetgetdata.data.token', "-", response, "-");
+    //     axios.get(`/api/user/info/${userId}`)
+    //         .then((response) => {
+    //             console.log('getgetgetdata.data.token', "-", response, "-");
                 
-                userDto.profileImg = response.data.user_image;
-                userDto.email = response.data.user_email;
-                userDto.age = response.data.user_age;
-                userDto.gender = response.data.user_gender;
-                userDto.school = response.data.school;
-                userDto.tech =  response.data.tech;
-                userDto.portname = response.data.port;
-                userDto.portsave = response.data.portsave_name;
-                console.log(userDto);
+    //             userDto.profileImg = response.data.user_image;
+    //             userDto.email = response.data.user_email;
+    //             userDto.age = response.data.user_age;
+    //             userDto.gender = response.data.user_gender;
+    //             userDto.school = response.data.school;
+    //             userDto.tech =  response.data.tech;
+    //             userDto.portname = response.data.port;
+    //             userDto.portsave = response.data.portsave_name;
+    //             console.log(userDto);
                 
-            })
-            .catch((error) => {
-                console.log(error);
-                console.log('실패');
-                return "error";
-            })
-            .finally(() => {
-                navigate('/portfolioviewer',{state: userDto});
-            });
+    //         })
+    //         .catch((error) => {
+    //             console.log(error);
+    //             console.log('실패');
+    //             return "error";
+    //         })
+    //         .finally(() => {
+    //             navigate('/portfolioviewer',{state: userDto});
+    //         });
             
             
-        }
+    //     }
 
     return (
         <div className="mx-auto w-9/12 px-4 mb-7 ">
@@ -131,14 +126,7 @@ function ViewPost() {
                     }}>완료하기</button>}
                     
                     {state.postType === "recruiting" && myPost && <button className = "ml-5 text-violet-500 " onClick={() => {
-
-                        console.log("postID : "+ postId);
                         navigate('/PostUpdate',{state:{id: postId}});
-                    
-                        // 글 쓰는 페이지로 이동하면서 현재 postId의 값을 다 가지고 와야함.
-                        // deleteMyPost(state.postId).then(()=>{
-                        //     navigate('/mainPage');
-                        // })
                     }}>수정하기</button>
                     }
 
@@ -199,22 +187,24 @@ function ViewPost() {
                 </div>
             </div>
 
-            {applicant.length !== 0 && state.postType === "recruiting" && myPost && <div className='mx-40 mt-7 mb-4 border-4 border-sky-200 rounded-2xl p-5 flex-column font-bold text-2xl max-h-96'>
-                <p>신청자 리스트</p>
+            {applicant.length !== 0 && state.postType === "recruiting" && myPost && <div className='mt-7 mb-4 border-4 border-sky-200 rounded-2xl p-5 flex-column font-bold max-h-96'>
+                <p className = "">신청자 리스트</p>
                 <hr className="h-px my-4 border-2 border-indigo-100 "></hr>
            
-                <div className="flex-column max-h-72 overflow-y-auto scrollbar-hide">
+                <div className="flex-column overflow-y-auto scrollbar-hide">
                     {applicant.map((item, idx) => {
+
                         return (
-                            <div key={idx} className="flex my-2 pb-1 border-b border-b-cyan-100">
+                            <div key={idx} className="text-2xl flex my-2 pb-1 border-b border-b-cyan-100">
                                 
-                                <p className="ml-1" onClick = {getPortInfo(item.id)}  >이름: {item.name}</p>
+                                <p className="ml-1">이름: {item.name}</p>
                                 <p className="ml-4">신청: {item.requested}</p>
-                                <div className ="ml-auto">
+
+                                <div className ="hidden md:flex md:text-2xl ml-auto">
                                     <button
                                         onClick = {approveModal}
-                                        value={item.name}
-                                        className="ml-4 border rounded-md w-24 bg-sky-100 outline-none hover:bg-sky-200">승인</button>
+                                        value={item.userId}
+                                        className="px-1 border rounded-md bg-sky-100 outline-none hover:bg-sky-200">승인</button>
                                        
                                     {approveModalOpen && <Modal open={approveModalOpen} close={closeModal2} header="승인하기" postId ={postId} category={clickedCategory} updateApplicant={updateApplicant} getCurrentCategory ={getCurrentCategory}>
                                         해당 인원을 승인시키겠습니까?
@@ -222,13 +212,13 @@ function ViewPost() {
 
                                     <button
                                         onClick ={()=>navigate("/portfolioview")}
-                                        value={item.name}
-                                        className="ml-4 border rounded-md w-24 bg-sky-100 outline-none hover:bg-sky-200">포트폴리오</button>
+                                        value={item.userId}
+                                        className="px-1 ml-4 border rounded-md  bg-sky-100 outline-none hover:bg-sky-200">포트폴리오</button>
                                         
                                     <button
                                         onClick = {rejectModal}
-                                        value={item.name}
-                                        className="ml-4 border rounded-md w-24 bg-sky-100 outline-none hover:bg-sky-200">거절</button>
+                                        value={item.userId}
+                                        className="px-1 ml-4 border rounded-md  bg-sky-100 outline-none hover:bg-sky-200">거절</button>
                                         
                                     {rejectModalOpen&&<Modal open={rejectModalOpen} close={closeModal3} header="거절하기" postId ={postId} category={clickedCategory} updateApplicant={updateApplicant}>
                                         해당 신청을 거절하시겠습니까?
