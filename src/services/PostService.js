fd1a92e6230b0333463219b3b67e1899526139f4
root <lmslmsms0616@gmail.com>
2023-02-07 10:33:35 +0900

    Update PostService.js

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -4,100 +4,99 @@ import { useRecoilValue } from 'recoil';
 
 
 const BACKEND_API_BASE_URL="http://210.109.62.6:8080";
-// const BACKEND_API_BASE_URL="http://localhost:8080";
-
-
 
 
 
 export async function getPosts(){
-    const response=await axios.get(`/api/post`);
-    
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/post`);
     return response.data.result;
 }
 
 export async function getMyPost(userId){
-    const response=await axios.get(`/api/post/${userId}`)
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/post/${userId}`)
     return response.data.result;
 }
 
 export async function getProject(userId){
-    const response=await axios.get(`/api/project/${userId}`)
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/project/${userId}`)
     return response.data.result;
 }
 
 export async function getRequestedPost(userId){
-    const response=await axios.get(`/api/requestedPost/${userId}`)
+    const response=await axios.get(`${BACKEND_API_BASE_URL}/api/requestedPost/${userId}`)
     return response.data.result;
 }
 
-export async function getTemperatures(){
-    const response = await axios.get(`/api/temperature`)
+export async function getPopularCategorys(){
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/popularCategory`)
     return response.data.result;
 }
 
 export async function getApplicant(postId){
-    const response = await axios.get(`/api/applicant/${postId}`)
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/applicant/${postId}`)
     return response.data.result;
 }
 
 export async function getCurrentPostCategory(postId){
-    const response = await axios.get(`/api/currentParticipant/${postId}`)
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/currentParticipant/${postId}`)
     return response.data.result;
 }
 
 // 신청하기 클릭 시 api
 export async function createApplicant(postId,request,userId){
-    const response = await axios.post(`/api/registerApplicant`, {
+    const response = await axios.post(`${BACKEND_API_BASE_URL}/api/registerApplicant`, {
         postId: postId,
-        userId: userId,
-        request: request
+        request: request,
+        userId: userId
     })
     return response.data.code;
 }
 
+
 // 승인하기 클릭 시-> 채팅방 참여 메세지 발송되어야 함.
-export async function createParticipant(postId,name){
-    const response = await axios.post(`/api/registerParticipant`, {
+
+export async function createParticipant(postId,userId){
+
+    const response = await axios.post(`${BACKEND_API_BASE_URL}/api/registerParticipant`, {
         postId: postId,
-        name: name
+        userId: userId
     })
     console.log("response",response);
     return response;
 }
 
-export async function deleteApplicant(postId,name){
-    const response = await axios.delete(`/api/applicant/${postId}/${name}`)
+export async function deleteApplicant(postId,userId){
+    const response = await axios.delete(`${BACKEND_API_BASE_URL}/api/applicant/${postId}/${userId}`)
     return response.data.code;
 }
 
 export async function getParticipant(postId,category){
-    const response = await axios.get(`/api/applicant/${postId}/${category}`)
+    const response = await axios.get(`${BACKEND_API_BASE_URL}/api/applicant/${postId}/${category}`)
     return response.data.result;
 }
 
 export async function deleteMyPost(postId, userId){
-    const response = await axios.delete(`/api/post/${postId}/${userId}`)
+    const response = await axios.delete(`${BACKEND_API_BASE_URL}/api/post/${postId}/${userId}`)
     return response.data.code;
 }
 
 export async function updatePostView(postId){
-    const response = await axios.patch(`/api/post/${postId}`)
+    const response = await axios.patch(`${BACKEND_API_BASE_URL}/api/post/${postId}`)
     return response.data.result;
 }
 
-export async function updateCurrentCategory(postId, name){
-    const response = await axios.patch(`/api/postCategory/${postId}/${name}`)
+export async function updateCurrentCategory(postId, userId){
+    const response = await axios.patch(`${BACKEND_API_BASE_URL}/api/postCategory/${postId}/${userId}`)
     return response.data.code;
 }
 
 export async function updatePostStatus(postId){
-    const response = await axios.patch(`/api/postStatus/${postId}`)
+    const response = await axios.patch(`${BACKEND_API_BASE_URL}/api/postStatus/${postId}`)
     return response.data.result;
 }
 
 export async function savePost(userId,postDto){
-    const response=await axios.post(`/api/post/save/${userId}`,postDto);
+    const response=await axios.post(`${BACKEND_API_BASE_URL}/api/post/save/${userId}`,postDto);
     console.log("response: ",response);
     return response;
 }
