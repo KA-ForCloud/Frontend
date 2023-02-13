6b5217056f2ef575fbf784fbde035ed1f75d9bb6
root <lmslmsms0616@gmail.com>
2023-02-13 21:15:03 +0900

    was test - getPosts

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -7,7 +7,7 @@ export const BACKEND_API_BASE_URL="https://teamchat.shop";
 
 
 export async function getPosts(){
-    const response=await axios.get(`/api/post`);
+    const response=await axios.get(`http://172.16.48.118:8080/api/post`);
     return response.data.result;
 }
 
