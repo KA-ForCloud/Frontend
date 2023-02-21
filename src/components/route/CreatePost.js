1d137a1399caa8fae8398aceb6db2d0042479104
root <lmslmsms0616@gmail.com>
2023-02-21 10:08:15 +0900

    Update CreatePost.js

--- src/components/route/CreatePost.js
+++ src/components/route/CreatePost.js
@@ -371,7 +371,7 @@ function CreatePost() {
 				else{
 					const chattingId=response.data.result;
 					const msg=users.name+"님이 입장하셨습니다.";
-					// enter(socket,chattingId,msg,users.id,users.name,getDate(),"msg");
+					enter(socket,chattingId,msg,users.id,users.name,getDate(),"msg");
 					navigate('/mainPage');
 				}
 			})
